60f7222c2deeaec70804db2a8ca406eb2c9b86ad
akurtovic4 <akurtovic4@etf.unsa.ba>
2022-06-12 13:18:18 +0200

    konfiguracija aplikacije za koristenje baze podataka

--- Sake/Sake/Data/ApplicationDbContext.cs
+++ Sake/Sake/Data/ApplicationDbContext.cs
@@ -8,9 +8,32 @@ namespace Sake.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            : base(options)
-        {
-        }
+       public ApplicationDbContext(DbContextOptions<ApplicationDbContext> 
+options)
+ : base(options)
+ {
+ }
+ public DbSet<Lutrija> Lutrija { get; set; }
+ public DbSet<Utakmica> Utakmica { get; set; }
+ public DbSet<GledanjeUtakmice> GledanjeUtakmice { get; set; }
+ public DbSet<Ulaznica> Ulaznica { get; set; }
+ public DbSet<Igrac> Igrac { get; set; }
+ public DbSet<SportskiTim> SportskiTim  { get; set; }
+ public DbSet<IgranjeZaTim> IgranjeZaTim { get; set; }
+       
+
+ protected override void OnModelCreating(ModelBuilder modelBuilder)
+ {
+ modelBuilder.Entity<Lutrija>().ToTable("Lutrija");
+ modelBuilder.Entity<Utakmica>().ToTable("Utakmica");
+ modelBuilder.Entity<GledanjeUtakmice>().ToTable("GledanjeUtakmice");
+ modelBuilder.Entity<Ulaznica>().ToTable("Ulaznica");
+ modelBuilder.Entity<Igrac>().ToTable("Igrac");
+ modelBuilder.Entity<SportskiTim>().ToTable("SportskiTim");
+ modelBuilder.Entity<IgranjeZaTim>().ToTable("IgranjeZaTim");
+           
+
+ base.OnModelCreating(modelBuilder);
+ }
     }
 }
